f8b9b7711904fd5b2c7f27e30f9a14936d23d1b5
Kenji Obata <1093913+kenjioba@users.noreply.github.com>
2021-12-27 13:56:33 -0700

    Delete turbo-run.ps1

--- turbo-run.ps1
+++
@@ -1,7 +0,0 @@
-param($apiKey, $app)
-
-$p = Start-Process -NoNewWindow -FilePath "${env:ProgramFiles(x86)}\turbo\cmd\turbo.exe" -ArgumentList "login --api-key=$apiKey" -PassThru
-$p.WaitForExit()
-
-$p = Start-Process -NoNewWindow -FilePath "${env:ProgramFiles(x86)}\turbo\cmd\turboplay.exe" -ArgumentList "turbo run $app" -PassThru
-$p.WaitForExit()
