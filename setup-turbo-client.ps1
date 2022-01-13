b7f2028260d860f8123aac1e4c331448f28c478c
Kenji Obata <1093913+kenjioba@users.noreply.github.com>
2022-01-12 18:54:26 -0700

    Mount to F: drive

--- setup-turbo-client.ps1
+++ setup-turbo-client.ps1
@@ -9,5 +9,6 @@ Out-File "$env:TEMP\attach2.txt" -InputObject "assign mount='d:'" -Encoding utf8
 Out-File "$env:TEMP\attach2.txt" -InputObject "exit" -Encoding utf8 -Append
 diskpart /s $env:TEMP\attach2.txt
 $env:TEMP
-$p = Start-Process -NoNewWindow -FilePath "d:\turbo-client-21.10.2402.0.exe" -ArgumentList "--silent --all-users --domain=$domain --add-trusted-source=$domain" -PassThru
+$clientexe = (Get-ChildItem "d:\*.exe" | Select-Object -First 1).name
+$p = Start-Process -NoNewWindow -FilePath "d:\$clientexe" -ArgumentList "--silent --all-users --domain=$domain --add-trusted-source=$domain" -PassThru
 $p.WaitForExit()
